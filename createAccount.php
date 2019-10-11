61c9067524f723784f37d3ef9531bcb66ba1db1a
Logan Hylton <37708431+lwh4wk@users.noreply.github.com>
2019-10-10 23:41:20 -0400

    added ajax js to signup to call createAccount.php to add a user to the database
    added ajax js to signup to call createAccount.php to add a user to the database, also changed some of createAccount.php to work with the database

--- createAccount.php
+++ createAccount.php
@@ -1,30 +1,32 @@
 <?php
 
-if (!_server['REQUEST_METHOD'] == 'POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     require("dbconnect.php");
     $email = htmlspecialchars($_POST['emailInput']);
-    $pwd = htmlspecialchars($_POST['passwordInput']);
+    $pwd = password_hash(htmlspecialchars($_POST['passwordInput']), PASSWORD_BCRYPT);
     $fname = htmlspecialchars($_POST['firstNameInput']);
     $lname = htmlspecialchars($_POST['lastNameInput']);
+    $city = htmlspecialchars($_POST['city']);
     $streetAddress = htmlspecialchars($_POST['streetAddress']);
     $state = htmlspecialchars($_POST['state']);
     $zipcode = htmlspecialchars($_POST['zipcode']);
 
-    $query = "INSERT INTO users (email, password, fname, lname, street, state, zipcode) VALUES (:email, :pwd, :fname, :lname, :streetAddress, :state, :zipcode)";
+    $query = "INSERT INTO users (email, password, fname, lname, street, city, state, zipcode) VALUES (:email, :pwd, :fname, :lname, :streetAddress, :city, :state, :zipcode)";
     $statement = $db->prepare($query);
-    $statement->bindValue(":email", $email);
-    $statement->bindValue(":pwd", $pwd);
-    $statement->bindValue(":fname", $fname);
-    $statement->bindValue(":lname", $lname);
-    $statement->bindValue(":streetAddress", $streetAddress);
-    $statement->bindValue(":state", $state);
-    $statement->bindValue(":zipcode", $zipcode);
+    $statement->bindValue(":email", "{" . $email . "}");
+    $statement->bindValue(":pwd", "{" . $pwd . "}");
+    $statement->bindValue(":fname", "{" . $fname . "}");
+    $statement->bindValue(":lname", "{" . $lname . "}");
+    $statement->bindValue(":streetAddress", "{" . $streetAddress . "}");
+    $statement->bindValue(":city", "{" . $city . "}");
+    $statement->bindValue(":state", "{" . $state . "}");
+    $statement->bindValue(":zipcode", intval($zipcode));
     $statement->execute();
     $statement->closeCursor();
 
     session_start();
-    $_SESSION['user'];
+    $_SESSION['user'] = $email;
 
+} else {
+    echo "test";
 }
-
-?>
