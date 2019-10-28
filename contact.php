1710ad8306e53d26ff6c43b4ae77182a88acae51
JChen9803 <37745376+JChen9803@users.noreply.github.com>
2019-10-27 23:56:31 -0400

    Changed contact to email track it support
    instread of emailing the sender, it sends an email to the trackit team email

--- contact.php
+++ contact.php
@@ -20,9 +20,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $mail->Username = 'trackitsales@gmail.com';
     $mail->Password = 'Trackit123';
     $mail->setFrom('sender@example.com');
-    $mail->addAddress($email);
-    $mail->Subject = 'Track It Support';
-    $mail->Body = "Hi " . $fname . " " . $lname . ",\r\nThanks for contacting us. You sent us the following message:\r\n\n" . $message . "\r\n\nWe will respond to your request as soon as possible.\r\n-Track It Support Team";
+    $mail->addAddress('trackitsales@gmail.com');
+    $mail->Subject = 'Contact from ' .  $fname . " " . $lname;
+    $mail->Body = "you have been contacted by " . $fname . " " . $lname . " with the following message:\r\n\n" . $message . "\r\n\nTheir email address is: " . $email;
     $mail->send();
 
 }
