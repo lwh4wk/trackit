8a8c1265257676a302352b25b8e90c5c122e3551
Logan Hylton <37708431+lwh4wk@users.noreply.github.com>
2019-10-27 22:49:55 -0400

    Added email
    added the email functionality after signing up

--- sendSignupEmail.php
+++ sendSignupEmail.php
@@ -1,3 +1,24 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'trackitsales@gmail.com';
+    $mail->Password = 'Trackit123';
+    $mail->setFrom('sender@example.com');
+    $mail->addAddress('lhyltoncs@gmail.com');
+    $mail->Subject = 'Subject';
+    $mail->Body = 'body';
+    $mail->send();
+} catch (Exception $e) {
+    echo "Message could not be send. Mailer Error: {$mail->ErrorInfo}";
+}
