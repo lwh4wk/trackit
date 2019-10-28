5fa63261ea635e5c18d950838fc519d1d8d279ce
Logan Hylton <lwh4wk@virginia.edu>
2019-10-28 14:19:40 -0400

    fixed sql
    fixed sql statement changed from user to users

--- changePassword.php
+++ changePassword.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $pass = substr($row[0], 1, strlen($row[0]) - 2);
         if (password_verify($currentPassword, $pass)) {
             $statement->closeCursor();
-            $query = "UPDATE user SET password=:pwd WHERE user=:userName";
+            $query = "UPDATE users SET password=:pwd WHERE user=:userName";
             $statement = $db->prepare($query);
             $statement->bindValue(':userName', $user);
             $statement->bindValue(':pwd', password_hash($newPassword, PASSWORD_BCRYPT));
