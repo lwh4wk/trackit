58402f1b8df279d363a1348cb3fa7ad8732a57b4
JChen9803 <37745376+JChen9803@users.noreply.github.com>
2019-10-28 02:34:06 -0400

    Created Basic Account page and change passowrd function
    Created a basic account page that will display your information and gives you the function to change your password

---
+++ changePassword.php
@@ -0,0 +1,34 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require("dbconnect.php");
+    session_start();
+
+    $user = $_SESSION['user'];
+
+    $currentPassword = $_POST['current'];
+    $newPassword = $_POST['new'];
+
+    $sql = "select password from users where email='{" . $user . "}'";
+    $statement = $db->prepare($sql);
+    $statement->execute();
+
+    if ($statement->rowCount() > 0) {
+        $row = $statement->fetch();
+        $pass = substr($row[0], 1, strlen($row[0]) - 2);
+        if (password_verify($currentPassword, $pass)) {
+            $statement->closeCursor();
+            $query = "UPDATE user SET password=:pwd WHERE user=:userName";
+            $statement = $db->prepare($query);
+            $statement->bindValue(':userName', $user);
+            $statement->bindValue(':pwd', password_hash($newPassword, PASSWORD_BCRYPT));
+            $statement->execute();
+            $statement->closeCursor();
+            echo "success";
+        } else {
+            echo "failed";
+        }
+    } else {
+        echo "no user";
+    }
+}
