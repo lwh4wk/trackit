b311a97b311cc325e8bfece38c9ae751071b65e0
JChen9803 <37745376+JChen9803@users.noreply.github.com>
2019-10-28 21:31:09 -0400

    Fixed contact page so to email the track it team
    Submitting the contact form now sends correctly to the right email address including all the given information

--- contact.php
+++ contact.php
@@ -20,9 +20,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $mail->Username = 'trackitsales@gmail.com';
     $mail->Password = 'Trackit123';
     $mail->setFrom('sender@example.com');
-    $mail->addAddress($email);
-    $mail->Subject = 'Track It Support';
-    $mail->Body = "Hi " . $fname . " " . $lname . ",\r\nThanks for contacting us. You sent us the following message:\r\n\n" . $message . "\r\n\nWe will respond to your request as soon as possible.\r\n-Track It Support Team";
+    $mail->addAddress('trackitsales@gmail.com');
+    $mail->Subject = 'Contacted By ' . $fname . " " . $lname;
+    $mail->Body = "Name: " . $fname . " " . $lname . "\r\nEmail: " . $email . "\r\n\nMessage:\r\n" . $message;
     $mail->send();
 
 }
